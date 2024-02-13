117a8a1b5fec5bf66ca404c9fad9cf1a23de29e6
ivponicka <56002673+ivponicka@users.noreply.github.com>
2024-02-13 20:03:35 +0100

    Updated

---
+++ Front-end-Skyline-Heights-Hotel/src/components/common/RoomTypeSelector.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+
+const RoomTypeSelector = () => {
+  const[RoomType, setRoomTypes] = useState([])
+  const[showNewRoomTypeInput, setShowNewRoomTypeInput] = useState(false)
+  const[newRoomType, setNewRoomType] = useState("")
+
+  useEffect
+  return (
+    <div>RoomTypeSelector</div>
+  )
+}
+
+export default RoomTypeSelector
