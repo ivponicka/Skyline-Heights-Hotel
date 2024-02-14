981c337af0d2f9713cdff6c257a233a31117ca3d
ivponicka <56002673+ivponicka@users.noreply.github.com>
2024-02-14 18:13:00 +0100

    Room type - added methods/hooks

--- Front-end-Skyline-Heights-Hotel/src/components/common/RoomTypeSelector.jsx
+++ Front-end-Skyline-Heights-Hotel/src/components/common/RoomTypeSelector.jsx
@@ -1,14 +1,68 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { getRoomTypes } from "../utils/ApiFunctions";
 
-const RoomTypeSelector = () => {
-  const[RoomType, setRoomTypes] = useState([])
-  const[showNewRoomTypeInput, setShowNewRoomTypeInput] = useState(false)
-  const[newRoomType, setNewRoomType] = useState("")
+const RoomTypeSelector = (handleNewRoomInputChange, newRoom) => {
+  const [roomTypes, setRoomTypes] = useState([]);
+  const [showNewRoomTypeInput, setShowNewRoomTypeInput] = useState(false);
+  const [newRoomType, setNewRoomType] = useState("");
 
-  useEffect
+  useEffect(() => {
+    getRoomTypes().then((data) => {
+      setRoomTypes(data);
+    });
+  }, []);
+
+  const handleNewRoomTypeInputChange = (e) => {
+    setNewRoomType(e.target.value);
+  };
+
+  const handleAddNewRoomType = () => {
+    if (newRoomType !== "") {
+      setRoomTypes([...roomTypes, newRoomType]);
+      setNewRoomType("");
+      setShowNewRoomTypeInput(false);
+    }
+  };
+
   return (
-    <div>RoomTypeSelector</div>
-  )
-}
+    <>
+      {roomTypes.length > 0 && (
+        <div>
+          <select
+            id="roomType"
+            name="roomType"
+            value={newRoom.roomType}
+            onChange={(e) => {
+              if (e.target.value === "Add new") {
+                setShowNewRoomTypeInput(true);
+              } else {
+                handleNewRoomInputChange(e);
+              }
+            }}
+          >
+            <option value={""}>Select a room type</option>
+            <option value={"Add new"}>Add new</option>
+            {roomTypes.map((type, index) => {
+              <option key={index} value={type}>
+                {type}
+              </option>;
+            })}
+          </select>
+          {showNewRoomTypeInput && (
+            <div className='input-group'>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Enter a new room type"
+                onChange={handleNewRoomTypeInputChange}
+              />
+              <button className="btn btn-hotel" type="button" onClick={handleAddNewRoomType}>Add</button>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default RoomTypeSelector
+export default RoomTypeSelector;
